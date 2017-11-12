aaa9b2f7b5cedb5b0d9d88c101e3f0ae4e8a2387
Brainya <bombird.dtb@gmail.com>
2017-11-12 22:09:31 +0900

    Initial Commit

---
+++ TestNewsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestNewsApp
+//
+//  Created by Hiroki on 2017/11/12.
+//  Copyright © 2017年 test inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
